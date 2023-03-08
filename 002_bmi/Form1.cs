f7cc12ba1d6737551b827d4149ab2f3513fbf5f2
sso9s <114461598+sso9s@users.noreply.github.com>
2023-03-08 09:20:29 +0900

    2023/03/08 수업내용

---
+++ 002_bmi/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _002_bmi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double height, weight;
+
+            height = double.Parse(textBox1.Text);
+            weight = double.Parse(textBox2.Text);
+
+            double bmi = weight / (height / 100 * height / 100);
+            label3.Text = "BMI = " + bmi;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
